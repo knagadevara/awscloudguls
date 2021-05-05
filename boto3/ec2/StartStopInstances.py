32bf260da779cead028fb57d7cf71a14f3b8364a
knagadevara <vnsk.1991@gmail.com>
2021-05-06 00:20:44 +0530

    Added argpaeser

--- boto3/ec2/StartStopInstances.py
+++ boto3/ec2/StartStopInstances.py
@@ -1,7 +1,35 @@
 #!/usr/bin/python3
 
+
+import argparse
 import boto3
 import json
+
+
+#### Helpful Content to display
+argDesc='Starts or Stops EC2 instances in all the regions'
+gen1="Request you to provide the below as arguments for the file: \n\t %(prog)s <stop/start>"
+epilog="And that's how you'd run a the script"
+
+#### Initialization of Parser to collect Data
+parseme = argparse.ArgumentParser(prog='StartStopInstances' ,  description=argDesc, usage=gen1 , epilog=epilog )
+parseme.add_argument('--InstanceAction', '-a', type=str, help='Takes only stop/start/terminate', required=True)
+arguments =  parseme.parse_args()
+
+def getSTATUS():
+    if arguments.InstanceAction == "start":
+        print("\t\tSTARTING")
+        return instance.start
+    elif arguments.InstanceAction == "stop":
+        print("\t\tSTOPPING")
+        return instance.stop
+    elif arguments.InstanceAction == "terminate":
+        print("\t\tTERMINATING")
+        return instance.terminate
+    else:
+        print(gen1)
+
+
 
 ## Making a client
 ec2client = boto3.client('ec2')
@@ -24,10 +52,6 @@ for region in regionNames:
         print("\t\tPlacement: {0}".format(PresentState))
         print("\t\tBlockDevices: {0} , VolumeID: {1}".format(instance.block_device_mappings[0]['DeviceName'] , instance.block_device_mappings[0]['Ebs']['VolumeId']))
         print("\t\tCPU: {0}".format(instance.cpu_options))
-        if PresentState['Code'] == 80:
-            instance.start()
-            print("\t\tSTARTING")
-        else:
-            instance.stop()
-            print("\t\tSTOPPING")
+        runME = getSTATUS
+        runME()
         print("\n\n")

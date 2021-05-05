667254ea70660c81a07640723dabc698f83b97f0
knagadevara <vnsk.1991@gmail.com>
2021-05-06 00:49:54 +0530

    removed terminate and added present state check validator

--- boto3/ec2/StartStopInstances.py
+++ boto3/ec2/StartStopInstances.py
@@ -10,25 +10,27 @@ import json
 argDesc='Starts or Stops EC2 instances in all the regions'
 gen1="Request you to provide the below as arguments for the file: \n\t %(prog)s <stop/start>"
 epilog="And that's how you'd run a the script"
+stateCODE =  0
 
 #### Initialization of Parser to collect Data
 parseme = argparse.ArgumentParser(prog='StartStopInstances' ,  description=argDesc, usage=gen1 , epilog=epilog )
-parseme.add_argument('--InstanceAction', '-a', type=str, help='Takes only stop/start/terminate', required=True)
+parseme.add_argument('--InstanceAction', '-a', type=str, help='Takes only stop/start', required=True)
 arguments =  parseme.parse_args()
 
-def getSTATUS():
+
+def getSTATUS(instance):
+    global stateCODE
     if arguments.InstanceAction == "start":
-        print("\t\tSTARTING")
+        stateCODE = 16
         return instance.start
     elif arguments.InstanceAction == "stop":
-        print("\t\tSTOPPING")
+        stateCODE = 80
         return instance.stop
     elif arguments.InstanceAction == "terminate":
-        print("\t\tTERMINATING")
-        return instance.terminate
+        stateCODE = 48
+        exit
     else:
         print(gen1)
-
 
 
 ## Making a client
@@ -43,15 +45,19 @@ for region in regionNames:
     #instances = ec2_resource.instances.filter( Filters=[{ 'Name' : 'instance-state-name' , 'Values': ['running']  }] )
     instances = ec2_resource.instances.all()
     print(region)
-    print("-----" * 6)
+    print("--- --- " * 6)
     for instance in instances:
+        print("\n\n")
         print("\tInstanceId: {0}".format(instance.id))
         print("\t\tInstanceType: {0}".format(instance.instance_type))
         print("\t\tPlacement: {0}".format(instance.placement))
         PresentState=instance.state
-        print("\t\tPlacement: {0}".format(PresentState))
+        print("\t\tPresentState: {0}".format(PresentState))
         print("\t\tBlockDevices: {0} , VolumeID: {1}".format(instance.block_device_mappings[0]['DeviceName'] , instance.block_device_mappings[0]['Ebs']['VolumeId']))
         print("\t\tCPU: {0}".format(instance.cpu_options))
-        runME = getSTATUS
-        runME()
-        print("\n\n")
+        runME = getSTATUS(instance)
+        if PresentState['Code'] != stateCODE:
+            runME()
+            print("\t\tExecuting {0}".format(arguments.InstanceAction))
+        else:
+            print("\t\tAlready in {0} state".format(PresentState))
